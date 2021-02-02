0f48cd4d0a6a019c927397a8c4fecd5bde7d19b7
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2021-02-02 21:51:30 +0100

    Delete password_hash.php

--- password_hash.php
+++
@@ -1,9 +0,0 @@
-<?php 
-ECHO nl2br("<b>" .password_hash("rasmuslerdorf",  PASSWORD_DEFAULT) . "</b> (valeur à stocker même si elle change avec le temps)\n");
-
-if (password_verify('rasmuslerdorf', '$2y$10$/rKg11J7VD7JgebwnrIhzud9jdKqTN6ICTnmCAoxiJsAPa4FF76HS')) {
-    echo 'Le mot de passe est valide !';
-} else {
-    echo 'Le mot de passe est invalide.';
-}
-?>
